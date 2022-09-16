5c1d8459194095e85ffedac8de401e0c38843d5e
tanphat02 <ledinhtanphat2@gmail.com>
2022-09-16 20:33:08 +0700

    first commit

---
+++ hooks/footer-extras.php
@@ -0,0 +1,7 @@
+<footer class="main-footer">
+    <div class="pull-right hidden-xs">
+      <b>Version</b> 1.1.0
+    </div>
+    <strong>KIẾN THỨC VIỆT &nbsp;<a href="https://www.kienthucviet.info">KIẾN THỨC VIỆT</a>.</strong> All rights
+    reserved.
+  </footer>
